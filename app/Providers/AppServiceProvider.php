785bca5692fec53c7e45262cd812f53a46f60f43
Mariannorio <rodriguezmarianoarg@gmail.com>
2024-09-07 19:01:03 -0300

    cambio para mysql

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-
+use Illuminate\Support\Facades\Schema; //Nose bien porque se agrego esto segun chtaGPT es por: use Illuminate\Support\Facades\Schema;: Se importa la clase Schema para poder trabajar con los métodos relacionados con el esquema de la base de datos.
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -19,6 +19,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Schema::defaultStringLength(191); //Nose bien porque se agrego esto segun chtaGPT es por: Schema::defaultStringLength(191);: Establece una longitud máxima de 191 caracteres para las columnas de tipo VARCHAR por defecto, asegurando la compatibilidad con versiones antiguas de MySQL y MariaD
     }
 }
