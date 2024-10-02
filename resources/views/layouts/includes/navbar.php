5488db04a5e65e9d00f9db152ac8276cb29de20b
CristianBarrios983 <barrioscristian.9908@gmail.com>
2024-10-01 21:12:43 -0300

    Vista mesas

---
+++ resources/views/layouts/includes/navbar.php
@@ -0,0 +1,35 @@
+<nav class="navbar navbar-expand-lg bg-dark" data-bs-theme="dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">ReservApp</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Inicio</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Mesas</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Reservas
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Reservar</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="#">Ver reservas</a></li>
+          </ul>
+        </li>
+
+      </ul>
+        <ul class="dropdown-menu">
+          <li><a class="dropdown-item" href="#">Cuenta</a></li>
+          <li><a class="dropdown-item" href="#">Rol:</a></li>
+          <li><hr class="dropdown-divider"></li>
+          <li><a class="dropdown-item" href="#">Salir</a></li>
+        </ul>
+    </div>
+  </div>
+</nav>
--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/mesas', function () {
+    return view('mesas');
+});
+
 Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
